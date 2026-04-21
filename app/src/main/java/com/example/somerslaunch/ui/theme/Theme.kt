b541db2d2ece8b5f5492e18f8a12aad93e58296e
EgorAks92 <110968588+EgorAks92@users.noreply.github.com>
2026-04-21 10:58:05 +0300

    Set Montserrat as global Compose typography

--- app/src/main/java/com/example/somerslaunch/ui/theme/Theme.kt
+++ app/src/main/java/com/example/somerslaunch/ui/theme/Theme.kt
@@ -52,7 +52,7 @@ fun SomersLaunchTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = AppTypography,
         content = content
     )
 }
